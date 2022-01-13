4221cafca66b3f6df6fd7e8791d00113c19514ca
xreztech003 <xreztech003@gmail.com>
2022-01-13 17:46:07 -0600

    Changed colors being used for each object, hardcoded the colors, modified the text and box lines to be larger

--- mainfile.py
+++ mainfile.py
@@ -18,6 +18,7 @@ import sys
 sys.path.append("..")
 from utils import label_map_util
 from utils import visualization_utils as viz_utils
+import custom_utils
 
 print("\nLoading Saved Model...")
 PATH_TO_MODEL = "new_model" # point to folder containing 'model' folder
@@ -28,11 +29,13 @@ category_index = label_map_util.create_category_index_from_labelmap(os.path.join
 
 print("\nBegin Live Image Predicting")
 while True:
-    ret, image_np = cap.read()    
+    ret, image_np = cap.read() 
+    if ret == False:
+        print("Error Reading Frame, skipping...")
+        continue
+    
     input_tensor = tf.convert_to_tensor(image_np)
     input_tensor = input_tensor[tf.newaxis, ...]
-    if input_tensor == None:
-        continue
 
     detections = detect_fn(input_tensor)
     num_detections = int(detections.pop('num_detections'))
@@ -42,7 +45,7 @@ while True:
 
     image_np_with_detections = image_np.copy()
 
-    viz_utils.visualize_boxes_and_labels_on_image_array(
+    custom_utils.visualize_boxes_and_labels_on_image_array(
         image_np_with_detections,
         detections['detection_boxes'],
         detections['detection_classes'],
@@ -52,7 +55,7 @@ while True:
         max_boxes_to_draw=200,
         min_score_thresh=0.65,
         agnostic_mode=False,
-        line_thickness=6)
+        line_thickness=8)
 
     cv2.imshow('object detection', cv2.resize(image_np_with_detections, (800,600)))
     if cv2.waitKey(25) == ord('q'):
